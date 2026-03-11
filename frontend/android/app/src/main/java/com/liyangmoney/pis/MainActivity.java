67b0ff202182b21bf3dd766a740a42aec96fdc0f
liyangmoney0o0 <liyangmoney123456@gmail.com>
2026-03-11 15:52:40 +0000

    fix: 在 MainActivity 中注册 ApkInstallerPlugin

--- frontend/android/app/src/main/java/com/liyangmoney/pis/MainActivity.java
+++ frontend/android/app/src/main/java/com/liyangmoney/pis/MainActivity.java
@@ -1,5 +1,14 @@
 package com.liyangmoney.pis;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // 注册自定义插件
+        registerPlugin(ApkInstallerPlugin.class);
+    }
+}
