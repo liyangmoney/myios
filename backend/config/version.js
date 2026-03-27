b8c8c86617f5b0b95de7143fc9781f062c4a6495
MyIOS Team <liyangmoney@example.com>
2026-03-27 23:38:24 +0800

    Bump version to 1.7.9

--- backend/config/version.js
+++ backend/config/version.js
@@ -1,10 +1,9 @@
 // APP 版本配置
 // 每次发布新版本时，修改这里的版本号和更新日志
 
-export const version = '1.7.8'
+export const version = '1.7.9'
 
-export const updateLog = `1. 修复评论区双进度条问题
-2. 应用内版本号与版本配置同步`
+export const updateLog = `1. 优化操作日志显示（补充描述不显示附件）`
 
 export const forceUpdate = false
 
